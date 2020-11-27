e7d351a54df414085d8ac728c05375f0c82b9dc5
rfrixou <rfrixo01@ucy.ac.cy>
2020-11-27 20:29:57 +0200

    Finished implementing About Activity, added to the menu as 'About Us'.

--- app/src/main/java/com/example/ece464_weatherapp/MainActivity.java
+++ app/src/main/java/com/example/ece464_weatherapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ece464_weatherapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
@@ -52,6 +54,21 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            Intent in = new Intent(this, AboutActivity.class);
+            startActivity(in);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
